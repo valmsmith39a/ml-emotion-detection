fd5a09838c40c83d3195ac6af3d395b9544f9cee
valmsmith39a <valmsmith39a@gmail.com>
2022-11-08 21:25:00 -0800

    Spring Boot Gradle project setup

---
+++ src/main/java/com/machinelearning/emotiondetection/EmotionDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.machinelearning.emotiondetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionDetectionApplication.class, args);
+	}
+
+}
